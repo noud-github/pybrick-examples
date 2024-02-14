4fc6506cf59a015f1e8d5c3c0ab7d93c44fd996d
Arnoud Thörig <39429596+noud-github@users.noreply.github.com>
2024-02-14 19:52:40 +0100

    Update train-vs-crocodile.py
    add dual motor setup

--- train-vs-crocodile.py
+++ train-vs-crocodile.py
@@ -8,39 +8,8 @@ from pybricks.parameters import Port
 from uerrno import ENODEV
 
 crocodile=True
+dual_motor=False
 
-#hub = TechnicHub()
-# Dictionary of device identifiers along with their name.
-device_names = {
-    # pybricks.pupdevices.DCMotor
-    1: "Wedo 2.0 Medium Motor",
-    2: "Powered Up Train Motor",
-    # pybricks.pupdevices.Light
-    8: "Powered Up Light",
-    # pybricks.pupdevices.Motor
-    38: "BOOST Interactive Motor",
-    46: "Technic Large Motor",
-    47: "Technic Extra Large Motor",
-    48: "SPIKE Medium Angular Motor",
-    49: "SPIKE Large Angular Motor",
-    65: "SPIKE Small Angular Motor",
-    75: "Technic Medium Angular Motor",
-    76: "Technic Large Angular Motor",
-    # pybricks.pupdevices.TiltSensor
-    34: "Wedo 2.0 Tilt Sensor",
-    # pybricks.pupdevices.InfraredSensor
-    35: "Wedo 2.0 Infrared Motion Sensor",
-    # pybricks.pupdevices.ColorDistanceSensor
-    37: "BOOST Color Distance Sensor",
-    # pybricks.pupdevices.ColorSensor
-    61: "SPIKE Color Sensor",
-    # pybricks.pupdevices.UltrasonicSensor
-    62: "SPIKE Ultrasonic Sensor",
-    # pybricks.pupdevices.ForceSensor
-    63: "SPIKE Force Sensor",
-    # pybricks.pupdevices.ColorLightMatrix
-    64: "SPIKE 3x3 Color Light Matrix",
-}
 # Try to get the device, if it is attached.
 try:
     device = PUPDevice(Port.A)
@@ -54,19 +23,27 @@ except OSError as ex:
 # Get the device id
 id = device.info()["id"]
 
-# Look up the name.
-try:
-    #print(Port.A, ":", device_names[id])
-    if id == 2:
-        print("train")
-        crocodile=False
-    elif id == 46:
-        print("crocodile")
-        crocodile=True
-except KeyError:
-    print(port, ":", "Unknown device with ID", id)
+if id == 2:
+    print("train")
+    crocodile=False
+    # Try to get the device, if it is attached.
+    try:
+        device = PUPDevice(Port.C)
+    except OSError as ex:
+        if ex.args[0] == ENODEV:
+            # No device found on this port.
+            print("Single Motor")
+        else:
+            raise
+    if (device.info()["id"] == 2):
+        dual_motor=True
 
-print(":" + str(crocodile))
+elif id == 46:
+    print("crocodile")
+    crocodile=True
+
+
+
 
 if crocodile :
     train = Motor(Port.A)
@@ -76,7 +53,9 @@ if crocodile :
     # Change 0.5 to something smaller for more realism.
     train.control.limits(acceleration = acceleration * 0.5)
 else:
-    train = DCMotor(Port.A)
+    train = DCMotor(Port.A,Direction.CLOCKWISE)
+    if dual_motor:
+        train2 = DCMotor(Port.C,Direction.COUNTERCLOCKWISE)
 
 remote = Remote()
 
@@ -93,11 +72,11 @@ while True:
         wait(10)
 
     # Depending on what was pressed, get the speed level.
-    if Button.RIGHT_PLUS in pressed:
+    if Button.LEFT_PLUS in pressed:
         STEP_NOW += 1
-    if Button.RIGHT_MINUS in pressed:
+    if Button.LEFT_MINUS in pressed:
         STEP_NOW -= 1
-    if Button.RIGHT in pressed:
+    if Button.LEFT in pressed:
         STEP_NOW = 0
 
     # Limit level to the maximum number of steps.
@@ -110,8 +89,12 @@ while True:
             train.run(max_speed * STEP_NOW // NUM_STEPS)
         else:
             train.dc(10 * STEP_NOW)
+            if dual_motor:
+                train2.dc(10 * STEP_NOW)
     else:
         train.stop()
+        if dual_motor:
+            train2.stop()
 
     # Wait for the button to be released.
     while pressed:
