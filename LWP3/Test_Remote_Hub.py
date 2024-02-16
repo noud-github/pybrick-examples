b3f5a164cf823e04e2f3694952e1c35a6443b992
Arnoud Thörig <39429596+noud-github@users.noreply.github.com>
2024-02-16 15:20:33 +0100

    Update Test_Remote_Hub.py

--- LWP3/Test_Remote_Hub.py
+++ LWP3/Test_Remote_Hub.py
@@ -2,48 +2,43 @@ from pybricks.hubs import TechnicHub
 from pybricks.pupdevices import Motor, ColorDistanceSensor
 from pybricks.parameters import Color, Port
 from pybricks.tools import wait
+from pybricks.tools import multitask, run_task
 
 # This imports the DuploTrain from the duplo.py file.
-from RemoteHub import Remote_Technic_Hub
+from RemoteHub import RemoteTechnicHub, RemoteMotor
 
 print("start")
 
 # Initialize the hub and devices. You can use any other hub too.
 hub = TechnicHub()
-#dial = Motor(Port.A)
 sensor = ColorDistanceSensor(Port.A)
-
-# Connect to the train.
-test = Remote_Technic_Hub()
-
-# These variables are used to monitor the angle and color state.
-
-last_color = Color.BLACK
-hub.light.on(Color.CYAN)
+test = RemoteTechnicHub()
+hub.light.on(Color.GREEN)
 test.light.on(Color.GREEN)
 wait(1000)
 
-test.light.on(Color.RED)
 
+print(str(test.battery.voltage()))
 
-test.drive( Port.A,  50)
-
+remotemotor = RemoteMotor(Port.A, test)
+remotemotor.dc(50)
 wait (2000)
+remotemotor.dc(0)
 
-test.drive( Port.A,  0)
+def main():
+    #global last_color
+    last_color = Color.BLACK
+    while True:
+        # If the measured color changed, play choo choo
+        # and set the hub and train light to match.
+        color = sensor.color()
+        if last_color != color:
+            last_color = color
+            hub.light.on(color)
+            test.light.on(color)
 
-while True:
-    # If the measured color changed, play choo choo
-    # and set the hub and train light to match.
-    color = sensor.color()
-    if last_color != color:
-        last_color = color
-        
-        #if color != Color.NONE:
-        #    train.choo_choo()
-        hub.light.on(color)
-        test.light.on(color)
-        
-    
+main()
+
+
     
 
