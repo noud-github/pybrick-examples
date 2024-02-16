75095311f9828a60bfb1e680746763ebe9d65c8b
Arnoud Thörig <39429596+noud-github@users.noreply.github.com>
2024-02-16 09:02:07 +0100

    Create Test_Remote_Hub.py

---
+++ LWP3/Test_Remote_Hub.py
@@ -0,0 +1,8 @@
+from pybricks.hubs import TechnicHub
+from pybricks.pupdevices import Motor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
+
+hub = TechnicHub()
+
