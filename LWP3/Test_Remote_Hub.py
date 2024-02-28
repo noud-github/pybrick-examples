5a61c11ed5872510dca1df874ca2cfc6bcca274f
Arnoud Thörig <39429596+noud-github@users.noreply.github.com>
2024-02-28 08:01:26 +0100

    Update Test_Remote_Hub.py

--- LWP3/Test_Remote_Hub.py
+++ LWP3/Test_Remote_Hub.py
@@ -5,20 +5,24 @@ from pybricks.tools import wait
 from pybricks.tools import multitask, run_task
 
 # This imports the DuploTrain from the duplo.py file.
-from RemoteHub import RemoteTechnicHub, RemoteMotor
+from RemoteHub import RemoteTechnicHub, RemoteMotor, RemoteMario
 
 print("start")
 
 # Initialize the hub and devices. You can use any other hub too.
 hub = TechnicHub()
 sensor = ColorDistanceSensor(Port.A)
+
+# connects but hangs
+#mario = RemoteMario()
+
+
 test = RemoteTechnicHub()
 hub.light.on(Color.GREEN)
 test.light.on(Color.GREEN)
 wait(1000)
 
-
-print(str(test.battery.voltage()))
+# print(str(test.battery.voltage()))
 
 remotemotor = RemoteMotor(Port.A, test)
 remotemotor.dc(50)
@@ -34,8 +38,24 @@ def main():
         color = sensor.color()
         if last_color != color:
             last_color = color
-            hub.light.on(color)
             test.light.on(color)
+            hub.light.on(color)
+        wait( 50)
+
+def main2():
+    last_color = Color.GREEN
+    while True:
+        # If the measured color changed, play choo choo
+        # and set the hub and train light to match.
+        if last_color == Color.GREEN:
+            color = Color.RED
+        if last_color == Color.RED:
+            color = Color.GREEN
+        if last_color != color:
+            last_color = color
+            test.light.on(color)
+            hub.light.on(color)
+        wait( 10)
 
 main()
 
